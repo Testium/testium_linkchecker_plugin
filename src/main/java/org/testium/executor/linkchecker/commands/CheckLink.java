14dfeb8976aa9bb255fff026cb4f69094f476de4
ArjanKranenburg <Arjan.Kranenburg@gmail.com>
2013-09-25 21:18:12 +0200

    Improving CheckLink

--- src/main/java/org/testium/executor/linkchecker/commands/CheckLink.java
+++ src/main/java/org/testium/executor/linkchecker/commands/CheckLink.java
@@ -4,6 +4,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+import net.sf.testium.executor.TestExecutionException;
 import net.sf.testium.executor.general.GenericCommandExecutor;
 import net.sf.testium.executor.general.SpecifiedParameter;
 import net.sf.testium.systemundertest.SutInterface;
@@ -23,10 +24,28 @@ public class CheckLink extends GenericCommandExecutor
 	public static final SpecifiedParameter PARSPEC_URL = new SpecifiedParameter( 
 			"url", String.class, "The URL to check", false, true, true, false );
 
+	private static final SpecifiedParameter PARSPEC_CONNECT_TIMEOUT = new SpecifiedParameter( 
+			"connectTimeout", Long.class, "timeout if it can't connect", true, true, true, false )
+			.setDefaultValue( 5000L ); //milli-seconds
+
+	private static final SpecifiedParameter PARSPEC_RESPONSE_TIMEOUT = new SpecifiedParameter( 
+			"responseTimeout", Long.class, "timeout if the page is too slow", true, true, true, false )
+			.setDefaultValue( 5000L ); //milli-seconds
+
+	/**
+	 * TODO
+	 * - method (HEAD or GET)
+	 * - followRedirections
+	 * - saveResponse
+	 * - allowedResponses
+	 */
+
 	public CheckLink( SutInterface iface ) {
 		super( COMMAND, "Will check if a link is alive (i.e. responds with 2xx)", iface, new ArrayList<SpecifiedParameter>() );
 
 		this.addParamSpec( PARSPEC_URL );
+		this.addParamSpec( PARSPEC_CONNECT_TIMEOUT );
+		this.addParamSpec( PARSPEC_RESPONSE_TIMEOUT );
 	}
 
 	@Override
@@ -35,37 +54,31 @@ public class CheckLink extends GenericCommandExecutor
 			throws Exception {
 
 		String url = (String) this.obtainValue(aVariables, parameters, PARSPEC_URL);
-		isLive(url);
+		int connectTimeout = (int) (long) (Long) this.obtainOptionalValue(aVariables, parameters, PARSPEC_CONNECT_TIMEOUT);
+		int responseTimeout = (int) (long) (Long) this.obtainOptionalValue(aVariables, parameters, PARSPEC_RESPONSE_TIMEOUT);
+
+		isLive(url, connectTimeout, responseTimeout);
 	}
 	
 	
-	private static void isLive(String link) throws Exception {
+	private static void isLive(String link, int connectTimeout, int responseTimeout) throws Exception {
 		HttpURLConnection urlConnection = null;
 		try {
 			URL url = new URL(link);
 			urlConnection = (HttpURLConnection) url.openConnection();
 			urlConnection.setRequestMethod("HEAD");
-			urlConnection.setConnectTimeout(5000); /*
-													 * timeout after 5s if can't
-													 * connect
-													 */
-			urlConnection.setReadTimeout(5000); /*
-												 * timeout after 5s if the page
-												 * is too slow
-												 */
+			urlConnection.setConnectTimeout(connectTimeout); 
+			urlConnection.setReadTimeout(responseTimeout);
+
 			urlConnection.connect();
 			String redirectLink = urlConnection.getHeaderField("Location");
 			if (redirectLink != null && !link.equals(redirectLink)) {
-				isLive(redirectLink);
+				isLive(redirectLink, connectTimeout, responseTimeout);
 			} else {
-				if ( urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK ) {
-System.out.println( "OK - Checked link " + link);
-				} else {
-System.out.println( "NOK - response " + urlConnection.getResponseCode() + " for " + link);
+				if ( urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK ) {
+					throw new TestExecutionException( "Got response " + urlConnection.getResponseCode() + " for " + link);
 				}
 			}
-		} catch (Exception e) {
-			throw e;
 		} finally {
 			if (urlConnection != null) {
 				urlConnection.disconnect();
